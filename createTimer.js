0e4830a10ae6e473aafd2de148e136e78e761afe
Will Jones <willjones2010@gmail.com>
2020-01-30 14:59:59 -0500

    createTimer refactor

--- createTimer.js
+++ createTimer.js
@@ -1,18 +1,12 @@
 // this is for coding quiz exercise. creating a timer
 
 var currentTime = 300
-var startButton = document.getElementById("#startButton")
 
 
-function startQuiz() {
-    setInterval(function () { decreaseOneSecondInTime() }, 1000);
-
-    //write the code to hide start screen div
-
-    // query document for div and save to variable
-    var startScreen = document.getElementById("startScreen")
-    // use js setAtrribute to set the class name to hide
-    startScreen.setAttribute('class', 'hide')
+function startTimer() {
+    setInterval(function () {
+        decreaseOneSecondInTime()
+    }, 1000);
 }
 
 function decreaseOneSecondInTime() {
@@ -21,12 +15,7 @@ function decreaseOneSecondInTime() {
     console.log(currentTime)
 }
 
-startQuiz()
+startTimer()
 
-// look up javascript event types
-
-
-// add event listener expects two parameters. ('event type', function)
-startButton.addEventListener('click', startQuiz)
 
 
