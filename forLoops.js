0457bbfb63b22552c32cac4a910ec4c3bb525adc
Will Jones <willjones2010@gmail.com>
2020-03-09 18:06:47 -0400

    forloops

---
+++ forLoops.js
@@ -0,0 +1,14 @@
+let array = ['cat', 'dog', 'fish', 'bird', 'alligator', 'monkey']
+
+// For Loops have 3 parameters
+// 1st parameter
+// - variable intended to change on every iteration of the loop
+// 2nd parameter
+// - if this condition is true continue to the next iteration of the loop
+// 3rd parameter
+// - how to manipulate the defined variable on each iteration of the loop
+for (let i = 0; i < array.length; i++) {
+    // here we use our i variable to access the array indexes 0 - 6 (0 - array.length)
+    console.log(array[i])
+}
+
