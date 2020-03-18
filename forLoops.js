4ef6dac4ad5261c721999888582b501914619a20
Will Jones <willjones2010@gmail.com>
2020-03-17 21:26:01 -0400

    rows and cols

--- forLoops.js
+++ forLoops.js
@@ -9,9 +9,17 @@ let listOfPets = ['cat', 'dog', 'fish', 'bird', 'alligator', 'monkey']
 // - how to manipulate the defined variable on each iteration of the loop
 
 
+let numbers = [1, 2, 3, 4, 5, 6, 7]
+
 
 for (let i = 0; i < listOfPets.length; i++) {
     // here we use our i variable to access the array indexes 0 - 5)
     console.log(listOfPets[i])
 }
+
+for (let x = 0; x < numbers.length; x++) {
+    console.log(numbers[x])
+}
+
+
 
