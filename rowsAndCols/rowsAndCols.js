f9dd2d93c59e7b3068fe62e706960d306aa8d589
Will Jones <willjones2010@gmail.com>
2020-03-26 13:14:53 -0400

    css

--- rowsAndCols/rowsAndCols.js
+++ rowsAndCols/rowsAndCols.js
@@ -1,13 +1,13 @@
 let button = document.getElementById("clickButton")
-let meter = document.getElementById("disk_c")
+let meter = document.getElementById("meterId")
 
 
 button.addEventListener("click", function () {
     if (meter.value === 10) {
         meter.value = 0
-        console.log("happiness reset")
+        console.log("understanding reset")
     } else {
         meter.value = meter.value + 1
-        console.log("happiness increased")
+        console.log("understanding increased")
     }
 })
