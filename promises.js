95663b5497b38f07a0676ff17c341d9ac25403a3
Will Jones <willjones2010@gmail.com>
2020-05-06 13:32:24 -0400

    promises

--- promises.js
+++ promises.js
@@ -1,3 +1,15 @@
+// promises are how we handle asychronous javascript code
+// synchronous = linear
+// asychronous = multiple threads
+
+// create promise
+// - this means the computer is promising your a return of some
+// data in the future
+// since computers are super fast all this happens in milliseconds
+// some time later data is returned and promise is fulfilled
+// if there was an error promise rejects with associated message
+
+
 let myPromise = new Promise((resolve, reject) => {
     let myName = "Bob"
     if (myName === "Will") {
@@ -7,9 +19,51 @@ let myPromise = new Promise((resolve, reject) => {
     }
 })
 
+// we can use .then to control asychronous functions from continuing until the 
+// promise is resolved.
 
 myPromise.then((res) => {
     console.log("Winner: " + res)
 }).catch((err) => {
     console.log('Catch: ' + err)
 })
+
+
+// Async functions always return a promise. If the return value of an async
+// function is not explicitly a promise, it will be implicitly wrapped in a promise.
+
+// For example, the following:
+
+async function name() {
+   return 1
+}
+// ...is equivalent to the synchronous (because Promise executor functions run synchronously):
+
+function name2() {
+   return Promise.resolve(1)
+}
+
+
+
+function resolveAfter2Seconds() {
+    return new Promise(resolve => {
+      setTimeout(() => {
+        resolve('resolved');
+      }, 2000);
+    });
+  }
+
+// Code after each await expression can be thought of as existing in a .then 
+// callback. In this way a promise chain is implicitly constructed, with the
+// return value the final link in the chain. 
+
+  
+  async function asyncCall() {
+    console.log('calling');
+    const result = await resolveAfter2Seconds();
+    console.log(result);
+    // expected output: 'resolved'
+  }
+  
+  asyncCall();
+  
